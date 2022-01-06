449b190c8392317b89e6bfd2c7dacc53ae1f8422
Oguz Demir <doguzkaann@gmail.com>
2022-01-07 02:14:30 +0300

    Increased responsiveness of dynamic list by defining classes in [li.classList]

---
+++ data.js
@@ -0,0 +1,146 @@
+let article_data = [
+    {'title': 'İslam Keyifli Bir Tarzdır',
+    'content': `İslam sadece helal-haram gibi hükümlerin olduğu bir din değil. İslam bir düşünce biçimi bir hayat felsefesi, yaşam tarzı.
+
+    Sokakta yürümekten, izlediğiniz filme dinlediğiniz müziğe kadar -bir hükmü değil-, bir estetik anlayışı, alternatif bir teklifi var.
+    
+    Kuran Şen Bir Kitaptır
+    
+    “Bir bu Kur’an sıkıntı çekesin diye indirmedik” yani “Mutlu olasın diye indirdik.”
+    
+    Kuran bir hüküm kitabı değil. Çoğunluğunu kıssalardan oluşuyor. Yani insanın yaşanmışlıkları, dünyevi ve tarihi tecrübesi. Tarih kitapları, hikayeler akıcı şeyler değil mi?
+    
+    Düşünceye, tefekküre davet ediyor. Kuran’ı düşünmek lazım. Tefsir etmek ya da hüküm çıkarmak ayrı bir iş. Hüküm çıkarmaya girince, iki ayet okuyan, kendini âlim sanıp birilerini kafir ilan ediveriyor.
+    
+    İslam hükümler üzerinden öğretiliyor. Böyle olunca sıkıcı hatta itici oluyor. İslam, keyf, estetik, edebiyat ve yaşanan hayatlar üzerinden öğretilse, tadına varamayız.
+    
+    Kuran bir bulmaca kitabı. Hz. Adem’le ilgili bütün ayetleri toplasanız, nasıl bir puzzle çıkıyor hiç denediniz mi? Parçaları birleştirip biraz da düşününce heyecanlı bir hikaye. Üstelik sizi eğlendirirken, cennete götürüyor.
+    
+    Eğlenceyi sadece pc oyunlarından anlıyorsanız, daha büyümeniz lazım. Heyecan veren oyunlar yerinde saydırır ama düşündüren oyunlar insanı büyütür.`},
+    {'title': 'Doğu ve Batı Edebiyatında Aşk Üzerine Bir Not',
+    'content': `Necmettin Turinay, Bilge Yayınevi’nden çıkan Ahmet Mithat Efendi’nin Müşahedat romanı için bir “Notlar” kısmı hazırlamış. Kitapta geçen birçok tabir (romans, kalenderlik, bank-ı osmani gibi) bilgisiz okuyucu için açıklanmış. Ben de lise müfretadından bildiğime kanaat getirdiğim “Kerem ile Aslı” maddesine baktım ki şöyle denmiş:
+
+    “Kerem ile Aslı hikayesinin kadın kahramanı Aslı, bir ermeni keşişinin kızıdır ve hıristiyandır. Visal anında Kerem’in çektiği bir “ah” ile ikisi de yanar. [Ö]nemli olan Kerem’in temiz ve ideal aşkı, Aslı’nın kararmış kalbini yumuşatmaya ve onun müslüman olmasına yeter. Bu hikayede insanın kaderini değiştiremeyeceği, gerçek sevginin tanrıda olduğu, insanın sevdiğine bu dünyada ulaşamayacağı verilmeye çalışılır.”
+    
+    Turinay Kerem ile Aslı’nın ilk mesajlarını belirtmiş: Kaderin kesinliği ve isteklere nailiyetin ancak ahirette olacağı. Bu bilgilerin ışığında, Kerem ile Aslı hikayesi bu kadar yaygınlaşmakla büyük bir amme hizmeti yapmış sayılabilir. Bu hikayede durum buysa, benzeri şekilde diğer hikayelerimizde de mesaj aramağa çalışalım. Mesela Ferhat ile Şirin’de: Önemli olan aşıkların kavuşması değildir, kavuşma bir gün gerçekleşir, aşk için ölünebilir. Leyla ile Mecnun’a gelirsek: aşık için sevgiliye sahip olmaya gerek yoktur, sevgilinin mahallesi veya mahallesinin köpeği bile onu hatırlatır, ki zaten O’dur, aşk için “almak” değil “vermek” esastır, öyle ki insan sevdiği şeyleri -mal varlığı, çevresinin ona bakışı ve hürmeti- unutarak sadece O’nu arar, aşk için ölünebilir ilh.
+    
+    Gördüğünüz üzere coğrafyamızda İslam tasavvuf felsefesiyle taban tabana benzerlik gösteren bir aşk tasviri yaygın. Yine Turinay’ın notlarından “Aşık Garib” maddesine bakıyoruz:
+    
+    “16-17. asırlarda doğmuş bir halk hikayesi. Diğer halk hikayelerinden farklı bir yönü, Aşık Garip’le sevgilisi Sanem’in birbiriyle kavuşmasıdır. Hikayede realist çizgiler hakim.”
+    
+    Yazar Şah İsmail ve Gülistan hikayesi için benzer şeyler söylemiş.[1] Demek ki realist aşk hikayeleri klasik kültürümüzde azınlıkta kalıyor. Çoğunlukla ümitsiz aşkların saflığı ve yüceliğinden bahsediyoruz. Doğu edebiyatında realist aşk hikayesi, sonunda kavuşma ile biten hikaye oluyor. Peki, bizde aşk hikayelerinden çok aşk romanları okunmağa başladığı zamanları düşünelim: Batı edebiyatının realizmi aşk hikayelerine ne yapmış olmalı?
+    
+    Realizm insanın aşk ile ilişkisine dönüp onu sorgulamış olmalı. Marcel Proust, Goriot Baba için şöyle diyor: [Romanın K]imi portreleri etkililiklerini belki de bu bayağılığa borçludur. Aslında, bayağı güdüleri kabul etmek istemeyen, lanetleyen, ayıklayan ve bu yüzden yüce sayılan kişilerde bile bu güdüler şekil değiştirmiş halde var olabilir. Ne olursa olsun, hırslı insanın bir ideal aşkı olsa bile, bu aşk, hırsa ilişkin düşüncelerini maalesef değiştirmemesi bir yana hayatının tamamı değil, çoğunlukla gençliğinin iyi bir dönemidir sadece (…) Dolayısıyla Rastignac’ın, Vandenesse’in şefkatli bir aşkını gördüğümüzde, (…) soğukkanlı, hırslı tipler oldukları, hayatları boyunca hesap yaptıkları, gençliklerine ait bu romanın[2] (…) daha sonra unutulduğu, onu ancak gerçekten unutmuş insanlara özgü tebessümle andıkları (…) bizzat kahramanın, Madam de Mortsauf’la yaşanan maceraya herhangi bir maceraymışçasına, hayatlarını anısıyla doldurmadığı için bir hüzün bile duymadan değindiğini bildiğimizden, müthiş güçlü bir gerçeklik duygusu buluruz.
+    
+    Burası romanın ne yaptığına dair bir kısımdı. Proust, yazarın bu romandaki hedeflerini açıklıyor ki az önce belirttiğim aşk hikayelerinin mesajlarıyla karşılaştırılmaya müsait: “[A]şkın geçici olduğunu, bir gençlik hatası olduğunu, günün birinde bu kadar önemsenmeyeceğini (…) en çarpıcı biçimde göstermek (…)”[3]
+    
+    Elbette bu bakış açısının yazarla bir alakası olmalı. Proust, Balzac’ın normal hayatını da bir roman gibi -yani hayali bir macera gibi- yaşadığını düşünüyor. Ayrıca hayata bakış tarzı bu olduğu için yazının devamında “belki de yazarın en soylu duyguları en bayağı biçimde algılaması bir avantaj, hatta temel koşuldu” diyor.
+    
+    Bu çeşit bir gerçekçilik mutlaka okuyucuyu, dönemi de etkileyecekti. Nitekim daha sonrasında “natüralist edebiyat” çıktı. Hayatın her türlü sahnesini -pis addedilenleri dahil- insana dair bazı “gerçekler” tespit etmek için teşhir eden bir akım zuhur etti. Böylece realizm -aşk duygusunun aleyhine de olsa- gerçekçi hikayeler ortaya koymada daha da ileri gitmiş oldu.
+    
+    Bu ilerleyişin sonu geldi mi? Açıkçası bilmiyorum. Fakat realist edebiyatta hakikaten fena bulduğum aşk hikayeleri var. Amok Koşucusu böyle. Aşk bu hikayede ihtirastan doğan psikolojik bir rahatsızlık olarak ele alınır. Kahraman hikayenin sonunda her şeyini yitirdiğini, sadece aşkını değil, aynı zamanda memuriyetini de kaybettiğini belirtiyor.[4] Kanaatimce bu eser -tabirimi mazur görünüz- ruhu itibariyle değil, ancak konusuyla bir aşk hikayesidir.
+    
+    Bizde de aşk romanları çokça yazıldı. Namık Kemal’de eski geleneğimizi, Halid Ziya’da ve Reşat Nuri’de realizmi, Ömer Seyfettin’de -hikayeleri için- natüralizmi görüyoruz. Fakat genel olarak, Yeni Türk edebiyatının aşk klasiklerinde realizm radikal seviyede değil.
+    
+    Sonuç olarak, aşk edebiyatında realizm sevgiliye kavuşmayı getiren güzel bir unsur olarak anlaşılabilir. Fakat Batı edebiyatında zaman içinde bunun aşk duygusunun aleyhine ürün verdiği görülüyor. Doğu edebiyatı realizm ile daha normal diyebileceğimiz bir münasebeti sürdürüyor.
+    
+    [1]Alıntılar: Ahmet Mithat Efendi, Müşahedat (İstanbul: Bilge Yayınevi, 1979), s. 474
+    
+    [2]Marcel Proust, burada kurguyu yazarın değil adeta kahramanların romanı olarak değerlendirmiş. Bu sebeple metinde “gençliklerine ait bu roman” diyor.
+    
+    [3]Alıntılar: Honore de Balzac, Goriot Baba (İstanbul: İletişim Yayınları, 2017), s. 327-8
+    
+    [4]Stephan Zweig, Amok Koşucusu (İstanbul: Türkiye İş Bankası Kültür Yayınları, 2018), s. 58`},
+    {'title':`THE THIRD GIRL, PART II`,
+    'content': `The “crime and clues” and “investigation” segments are very much intertwined. We witness many people’s perspectives as we don’t follow one protagonist/narrator. Each point of view provides us with different clues respectively. The distribution of the clues is very similar to The Murders in The Rue Morgue, as we confront numerous clues and suspects which seem dubious. Although one of the characters we follow is Poirot, his thought process is not revealed to the viewer until the denouement. As a result, we’re discombobulated with an overwhelming amount of clues, suspects, and connections. Nonetheless, TTG differs from Rue Morgue in the manner it deals with the clues and suspects which we could refer to as loose ends. When the former’s way of revealing the orangoutan as the perpetrator was received as elementary; the latter efficiently ties up those loose ends in the denouement by linking each piece of evidence or doubt to the respective character -as did many crime-fiction works succeeding Rue Morgue- doing away with the viewers’ doubts and suspicions. However, it’s important to note that the multiple perspectives technique is derived from Wilkie Collins rather than E. A. Poe, as stated by Cawelti. I believe TTG executed this principal well, left no room for plot holes. When I reflect on what could be mentioned as a negative regarding the clues, I can’t come up with a convincing argument. There are some convenient coincidences like Ms. Sonia actually being in love with Sir Roderick, or Claudia Reece-Holland having a relationship with Andrew Restarick yet having no part in the scheme. However, if these clues actually panned out and Sonia and Claudia were the actual perpetrators, the denouement would have been anti-climactic as I would’ve reached the conclusion before Poirot, as would many others.
+
+    The announcement-explanation of the solution and the denouement are interbedded within each other as well. After Norma’s supposedly dead body is carried out of the house, everyone in the party is gathered in one small room. Poirot seizes the opportunity and begins demonstrating his thought procedure by raising questions –which I believe he had to answer for himself during the investigation- about people in the room, and denouncing those who are guilty while simultaneously erasing our doubts by process of elimination. Throughout this segment, the camera specifically focuses on the facial expressions of Ms. Cary/Battersby, Frances thereafter, and she ultimately turns out to be the murderer of Nanny Seagram along with the help of the impostor, Robert Orwell. When I first watched the scene, I didn’t realize that the camera was particularly interested in showing me how worried Frances was, but upon second viewing I understood that the camera work was a smart way of foreshadowing the climactic reveal. The main reason for me to not understand that the camera was used for foreshadowing is because TTG never really pays attention to Frances. Ariadne Oliver even refers to her as superficial when telling Poirot about her, persuading us that her character is not of much importance. This is a ploy which aims to surprise the viewer by revealing the least likely person to be guilty, as Cawelti states.
+
+    It turns out that Robert Orwell had the intention to take over the business, but after seeing that the business was worthless and of no use to him, he settled on getting Andrew Restarick’s share of the fortune left over by Norma’s mother. Frances, on the other hand, had the intention to revenge upon her father who had abandoned her. However, when she found out that her father was dead, she decided to conspire against Norma, her half-sister, who was more privileged than herself. The plan she had was to exploit Norma’s childhood tragedy of her mother committing suicide, to abuse the guilt she felt regarding her mother’s death, and to inevitably drive her insane.
+
+    As Poirot is explaining the way he solved the insoluble mystery, we see him get emotional for the first time. He expresses anger and disgust toward the criminals, and it becomes clear that he does this because he believes he is keeping a promise, which is a righteous thing to do. While this may seem like a departure from the convention in which the detectives only marvel at the intellectual value of a case, it really is not. This showcase of emotion does not take away from the wonder of the investigation. Poirot only bursts with anger and loathing in the denouement, which actually enhances the excitement of the segment. The crime is finally solved, and we can deduce that Poirot is just as excited as we are. After the said display of emotion, the criminals are apprehended, and we can see Poirot on the balcony with Ariadne Oliver. Poirot says as he looks at Norma hugging David Baker: “Am I the solver of puzzles with a heart that is cold? Or are we looking at the greatest of mysteries that life ever throws out? The mystery that even I, Hercule Poirot, will never be able to solve: the nature of love.” This plays with the idea of detectives being cold, mystery-solving machines, but at the same time, being human to the extent that their genius allows them.
+
+    The last sequences I mentioned were of great significance in terms of making the detective even more likable as a character. Throughout the investigation he is very rational, doesn’t let his feelings manifest themselves in any shape or form, but we are certain that they did motivate him in solving the case, as he says in the denouement: “…but I, Hercule Poirot, had promised to himself to save her.” This becomes even more apparent in the balcony scene when he says: “She smiles” with his eyes watering up and a smile on his face. We can infer from the aforementioned scenes that Poirot differs from his counterparts in the crime-fiction genre, in the sense that he’s not only a nonchalant genius, but he’s a genuinely good person that demands to seek justice. His motivations are not intellectual satisfaction, but emotional as well, they are righteous.
+
+    While I was affected by Poirot’s emotions and impressed with David Suchet’s performance, I unfortunately can’t say the same thing about Norma. I was not compelled enough to care much for her struggles, as the actress’ performance was not quite as good as I would have wanted it to be. This is actually the case for each and every character other than Hercule Poirot, Ariadne Oliver, and Sir Roderick. I was not very emotionally invested in the characters. While Cawelti does say that connecting too much with characters other than the detective mitigates the investigation’s effectiveness for the reader/viewer, I believe this is the incorrect way to do it. Characters are not very interesting, and they only exist to fill in the plot holes. The fact that there are so many characters causes some cluttering around the investigation, and we are distracted too much, as I personally couldn’t keep up with the amount of information I was receiving. In my opinion, the correct way is demonstrated in The Purloined Letter, in which the situation is handled very cleanly, as there’s no need for extra information and clues to baffle the viewer. The only thing that confuses us is the fact that every place imaginable has been thoroughly searched, yet there’s no sign of the letter.
+
+    To conclude, Agatha Christie’s Poirot: The Third Girl is entrenched in the guidelines of its genre, taking inspiration primarily from the architect of the said genre –E.A. Poe- while it does look for some wiggle space in the case of depicting the detective. It mainly succeeds in telling us a compelling detective investigation. We can say that it’s one of the examples of text-book golden age detective fiction.`},
+    {'title':`BİZE VARDIK`,
+    'content': `Daha çok erken.
+
+    Misafir hazırlığı telaşı halinde içimizde inat.
+    
+    İçindekini göster bana, şeklinle barış ve iste.
+    
+    Artık kavganı iste, alın terini özlediğin için iste.
+    
+    Çuvaldızımız yoktu ama biz bizeydik,
+    
+    örtümüz ısıtırdı, ayaklarımız açık değildi,
+    
+    sözümüz sözdü, fazlasında gözümüz yoktu;
+    
+    zaten fazlaydık,
+    
+    damla damla idrak göl olurken yorgunluk bizi aldı,
+    
+    hatırlattı, bir yerde karşılık vardı.
+    
+    Kaç kişiydik?
+    
+    İmkana şükreden, hor görmeyen kaç kişi?
+    
+    Yollar kapanmış, araçlar istop etmişken yüreği açık, kaç kişiydik?
+    
+    Mazeretimiz aş idi, sığınağımız aşk.
+    
+    Bir kasaba yetmiyordu dünyayı tanımak, anlamak, anlatmak için.
+    
+    Satacak, dağıtacak, paylaşacak çok malımız vardı.
+    
+    İşimiz, kıvancımız çoktu.
+    
+    Sayımız belli belirsiz olsa da ürkmezdik tenhalarda.
+    
+    Geniş zamanlı ma’nâ, soframızdaki kardeşlik,
+    
+    hemhâl oluş, diğergamlık;
+    
+    kederi kovmak içindi tebessüm.
+    
+    Kim öğretmişti bize dosdoğru olmayı,
+    
+    onu hatırlayalım daima,
+    
+    onu övelim, şanını yüceltelim,
+    
+    geride kalmadan yarı yolda, birlikte gidelim.
+    
+    Sonra biz geldik diyelim en yakın ve en uzak kasabalara,
+    
+    kalkın gidiyoruz diyelim, hazırlanın, biz geldik,
+    
+    sizle tanıştık, halleştik, onlara gidiyoruz.
+    
+    Bu barikatlar yıkılmalı.
+    
+    Ufkumuzu, sahramızı örten dumanlar yok olmalı.
+    
+    Biz geldik, burada onları bulduk, şunlar kaldı.
+    
+    Geldiğimiz diyarlardan selam getirdik.
+    
+    Bir de katığımızı aşınıza katmaya geldik.
+    
+    Bir de aşkımızı ay ışığında ısıttık, getirdik.
+    
+    Bizler her yerdeyiz.
+    
+    Bekleriz, karşılarız, ağırlarız onları yarı yolda.
+    
+    Kuşlar, kediler, köpekler açmış, yardıma muhtaçmış o diyarlarda.
+    
+    Biz gidiyoruz, onlar bekliyor, siz de gelin, tanış olmaya.`},
+];
+
+export {article_data};
